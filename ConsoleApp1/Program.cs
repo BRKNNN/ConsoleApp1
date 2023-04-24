722c4ae2f393baf27718a61a2fe304535f69adce
Буркин Максим Андреевич <burkin.ma1625@okeit.edu>
2023-04-24 10:39:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using CalculationLib;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculationLib calculation = new CalculationLib();
+            int a = 15;
+            int b = 5;
+            Console.WriteLine(" a = {0}, b = {1}", a, b);
+            Console.WriteLine(" a + b = {0}", calculation.Add(a, b));
+            Console.WriteLine(" a - b = {0}", calculation.Subtract(a, b));
+            Console.WriteLine(" a * b = {0}", calculation.Multiply(a, b));
+            Console.WriteLine(" a / b = {0}", calculation.Divide(a, b));
+
+
+        }
+    }
+}
